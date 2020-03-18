28665f645182b36dd36be1a4add5933d50f61f4e
Knarik <knarik.yeritsyan@gmail.com>
2020-03-19 01:14:45 +0400

    fixing server environment issues

--- libs/Router.php
+++ libs/Router.php
@@ -12,8 +12,8 @@ class Router
         }
         else {
             $url = explode('/', $url);
-            $controller = 'controllers\\' . ucfirst($url[0]);
-            var_dump($controller);die;
+            $controller = '\controllers\\' . ucfirst($url[0]);
+            var_dump(class_exists($controller));die;
             if (class_exists($controller)) {
                 $request->controller = '\controllers\\'.ucfirst($url[0]);
                 if (isset($url[1])) {
